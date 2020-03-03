7193ddecd7e14c944e171165e46d7fb96ce4599d
Luckijig <luc.jiang@free.fr>
2020-03-03 11:55:49 +0100

    ex1 - Emprunt

--- src/main/java/model/Livre.java
+++ src/main/java/model/Livre.java
@@ -1,11 +1,17 @@
 package main.java.model;
 
 public class Livre{
+    /*!
+	 *	CHAMPS DE LA TABLE MEMBRE
+	 */
     private int id;
     private String titre;
     private String auteur;
     private String isbn;
 
+    /*!
+	 *	CONSTRUCTEURS
+	 */
     public Livre() {
         this.id = 0;
         this.titre = "Bible";
@@ -20,11 +26,17 @@ public class Livre{
         this.isbn = isbn;
     }
 
+    /*!
+	 *	ACCESSEURS
+	 */
     public int getId() {return id;}
     public String getTitre() {return titre;}
     public String getAuteur() {return auteur;}
     public String getIsbn() {return isbn;}
 
+    /*!
+	 *	MODIFIEURS
+	 */
     public void setId(int id) {this.id = id;}
     public void setTitre(String titre) {this.titre = titre;}
     public void setAuteur(String auteur) {this.auteur = auteur;}
