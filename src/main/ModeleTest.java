bb8c3a1d586b88c395c5d9d6d900220dc89a412b
Luckijig <luc.jiang@free.fr>
2020-03-03 11:07:37 +0100

    ex1 - Livre et ModeleTest

---
+++ src/main/ModeleTest.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.java.model.*;
+
+public class ModeleTest{
+
+    static void main() {
+        Livre livreTest = new Livre();
+        System.out.println(livreTest.toString());
+    }
+}
--- src/main/java/model/Livre.java
+++ src/main/java/model/Livre.java
@@ -1,6 +1,37 @@
-package model;
+package main.java.model;
 
 public class Livre{
     private int id;
+    private String titre;
+    private String auteur;
+    private String isbn;
+
+    public Livre() {
+        this.id = 0;
+        this.titre = "Bible";
+        this.auteur = "Beaucoup de gens";
+        this.isbn = "Je suis l'ISBN de la Bible";
+    }
     
+    public Livre(int id, String titre, String auteur, String isbn) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+    }
+
+    public int getId() {return id;}
+    public String getTitre() {return titre;}
+    public String getAuteur() {return auteur;}
+    public String getIsbn() {return isbn;}
+
+    public void setId(int id) {this.id = id;}
+    public void setTitre(String titre) {this.titre = titre;}
+    public void setAuteur(String auteur) {this.auteur = auteur;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+
+    public String toString() {
+        String res = "Id : " + String.valueOf(id) + ", Titre : " + titre + ", Auteur : " + auteur + ", ISBN : " + isbn;
+        return res;
+    }
 }
