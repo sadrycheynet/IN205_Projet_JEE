7a2acaadea761bc05ae3ef01857c70f628e6c60c
sadrycheynet <sadry.cheynet974@gmail.com>
2020-03-22 18:28:45 +0100

    Implémentation des servlets Membres

---
+++ src/main/java/com/excilys/librarymanager/servlet/MembreDeleteServlet.java
@@ -0,0 +1,81 @@
+package com.excilys.librarymanager.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.librarymanager.exception.ServiceException;
+
+import com.excilys.librarymanager.model.Membre;
+
+import com.excilys.librarymanager.service.MembreService;
+import com.excilys.librarymanager.service.MembreServiceImpl;
+
+public class MembreDeleteServlet extends HttpServlet {	
+	
+	/*!
+	 * Cette méthode redirige sur la JSP membre_delete permettant de supprimer le membre observé
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MembreService membreService = MembreServiceImpl.getInstance();
+		
+		String inputId = request.getParameter("id");
+		int id = -1;
+		
+		Membre membre;
+		
+		try{
+			id = String.valueOf(inputId);
+			membre = membreService.getById(id);
+			
+		} catch(ServiceException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			membre = new Membre();
+			
+		} catch(NumberFormatException ebis){
+			membre = new Membre();		//Je ne suis pas sûr que cela soit utile, mais dans le doute, ça coûte pas tant que ça d'initialiser "membre"
+			throw new ServletException("Erreur lors du parsing : id="+inputId,ebis.getMessage());
+		}
+		
+		request.setAttribute("membre", membre);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/membre_delete.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	/*!
+	 * Cette méthode redirige sur la JSP membre_list permettant d'observer la liste des membres
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MembreService membreService = MembreServiceImpl.getInstance();
+		
+		String inputId = request.getParameter("id");
+		int id = -1;
+		
+		Membre membre;
+		
+		try{
+			id = String.valueOf(inputId);
+			membre = membreService.getById(id);
+			membreService.delete(membre);
+			
+		} catch(ServiceException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			membre = new Membre();
+			
+		} catch(NumberFormatException ebis){
+			membre = new Membre();		//Je ne suis pas sûr que cela soit utile, mais dans le doute, ça coûte pas tant que ça d'initialiser "membre"
+			throw new ServletException("Erreur lors du parsing : id="+inputId,ebis.getMessage());
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/membre_list.jsp");
+		dispatcher.forward(request, response);
+	}
+}
